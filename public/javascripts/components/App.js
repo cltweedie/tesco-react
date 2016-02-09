2b4fd3a7832a0783793d7144ad3a321e2f17d194
Chris Tweedie <cltweedie@gmail.com>
2016-02-09 14:54:27 +0000

    Add ResultBox class

--- public/javascripts/components/App.js
+++ public/javascripts/components/App.js
@@ -28,43 +28,7 @@ var App = React.createClass({
   }
 });
 
-var ResultsSection = React.createClass({
-  render: function() {
-    console.log(this.props.results);
-    if (this.props.results.length > 0) {
-      var resultsMarkup = this.props.results.map(function(result) {
-        return (
-          <section className="col-md-3" key={result['BaseProductId']}>
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <a href={"http://www.tesco.com/groceries/product/details/?id=" + result['ProductId']}>{result['Name']}</a>
-              </div>
-              <div className="panel-body">
-                <img className="col-md-6" src={result['ImagePath']} />
-                <div className="col-md-6" >
-                  <div className="price">
-                  £{result['Price'].toFixed(2)}
-                  <a className="buy-link" href={"http://www.tesco.com/groceries/product/details/?id=" + result['ProductId']}>Buy</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-        )
-      });
-      return (
-        <div>{resultsMarkup}</div>
-      )
-    } else {
-      return (<div></div>)
-    }
-  }
-});
-
 var SearchForm = React.createClass({
-  search: function(searchTerm) {
-
-  },
   handleSubmit: function(e) {
     e.preventDefault();
     var searchTerm = ReactDOM.findDOMNode(this.refs.searchTerm);
@@ -79,6 +43,47 @@ var SearchForm = React.createClass({
           <button className="btn btn-primary" >Search</button>
         </div>
       </form>
+    )
+  }
+});
+
+var ResultsSection = React.createClass({
+  render: function() {
+    if (this.props.results.length > 0) {
+      var resultsMarkup = this.props.results.map(function(result) {
+        return (
+          <ResultBox result={result} />
+        )
+      });
+      return (
+        <div>{resultsMarkup}</div>
+      )
+    } else {
+      return (<div></div>)
+    }
+  }
+});
+
+var ResultBox = React.createClass({
+  render: function() {
+    var result = this.props.result;
+    return (
+      <section className="col-md-3" key={result['BaseProductId']}>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <a href={"http://www.tesco.com/groceries/product/details/?id=" + result['ProductId']}>{result['Name']}</a>
+          </div>
+          <div className="panel-body">
+            <img className="col-md-6" src={result['ImagePath']} />
+            <div className="col-md-6" >
+              <div className="price">
+              £{result['Price'].toFixed(2)}
+              <a className="buy-link" href={"http://www.tesco.com/groceries/product/details/?id=" + result['ProductId']}>Buy</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     )
   }
 });
